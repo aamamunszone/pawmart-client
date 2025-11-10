7b3bca62bd134acf4dd19931f891f3efade21d56
Abdullah Al Mamun <aamamunszone@gmail.com>
2025-11-10 07:41:11 +0600

    update: HomePage Section updated

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import HeroSection from '../../components/home/HeroSection/HeroSection';
 import CategorySection from '../../components/home/CategorySection/CategorySection';
+import RecentListings from '../../components/home/RecentListings/RecentListings';
+import WhyAdopt from '../../components/home/WhyAdopt/WhyAdopt';
+import PetHeroes from '../../components/home/PetHeroes/PetHeroes';
 
 const Home = () => {
   return (
@@ -15,6 +18,21 @@ const Home = () => {
       {/* Category Section */}
       <section>
         <CategorySection />
+      </section>
+
+      {/* RecentListings Section */}
+      <section>
+        <RecentListings />
+      </section>
+
+      {/* WhyAdopt Section */}
+      <section>
+        <WhyAdopt />
+      </section>
+
+      {/* PetHeroes Section */}
+      <section>
+        <PetHeroes />
       </section>
     </div>
   );
